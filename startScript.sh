d636d3fcf0fed886444680ac2245eba0cae71c8b
AWireman <andrewwireman4@gmail.com>
2020-07-03 21:57:37 +0000

    dockerize VSCodeServer

--- startScript.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-systemctl disable systemd-resolved.service
-systemctl stop systemd-resolved.service
-docker start bb761292413c
-systemctl enable systemd-resolved.service
-systemctl stop systemd-resolved.service
